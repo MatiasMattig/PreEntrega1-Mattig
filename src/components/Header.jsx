352dbc13d741e6377109dc8538782fc541b8c47f
Matias Mattig <matu150503@gmail.com>
2023-07-07 10:44:37 -0300

    Cambios

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+export const Header = () => {
+
+    return (
+        <header className="header">
+            <div className="headerContainer">
+                <h1 className="headerTitle">MELOSHOP</h1>
+
+                <nav className="navbar">
+                    <a className="navbarLink" href="#">Enlace 1</a>
+                    <a className="navbarLink" href="#">Enlace 2</a>
+                    <a className="navbarLink" href="#">Enlace 3</a>
+                </nav>
+            </div>
+        </header>
+    )
+
+}
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+const ItemListContainer = ({saludo}) => {
+
+    return (
+        <div>
+            <h2>Item List Container</h2>
+
+            <p>Hola mundo</p>
+            <p>{saludo}</p>
+        </div>
+    )
+}
+
+export default ItemListContainer
