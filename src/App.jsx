62fc634a51485ab9c33daf73ed95f6897518d324
Matias Mattig <matu150503@gmail.com>
2023-07-07 13:07:56 -0300

    Cambio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import { Header } from './components/Header'
-import ItemListContainer from './components/ItemListContainer'
+import { Header } from './components/Header/Header'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 function App() {
 
