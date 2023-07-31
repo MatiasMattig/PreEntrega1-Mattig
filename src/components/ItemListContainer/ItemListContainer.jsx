73554d254a0c7b8a69a7983b4da810cc0957bdad
Matias Mattig <matu150503@gmail.com>
2023-07-31 13:11:19 -0300

    commit 8

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,90 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import '../../styles/components/ItemListContainer/ItemListContainer.scss';
-
-const MOCK_DATA = [
-    {
-        id: 1,
-        nombre: 'Producto 1',
-        descripcion: 'rewrfwef',
-        precio: 1500,
-        img: "daasd"
-    },
-    {
-        id: 2,
-        nombre: 'Producto 2',
-        descripcion: 'rewrfwef',
-        precio: 2500,
-        img: "daasd"
-    },
-    {
-        id: 3,
-        nombre: 'Producto 3',
-        descripcion: 'rewrfwef',
-        precio: 3500,
-        img: "daasd"
-    },
-    {
-        id: 4,
-        nombre: 'Producto 4',
-        descripcion: 'rewrfwef',
-        precio: 4500,
-        img: "daasd"
-    },
-    {
-        id: 5,
-        nombre: 'Producto 5',
-        descripcion: 'rewrfwef',
-        precio: 5500,
-        img: "daasd"
-    },
-    {
-        id: 6,
-        nombre: 'Producto 6',
-        descripcion: 'rewrfwef',
-        precio: 6500,
-        img: "daasd"
-    },
-];
-
-const pedirDatos = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(MOCK_DATA);
-    }, 2000);
-  });
-};
+import { pedirDatos } from '../../helpers/pedirDatos';
+import { useProductos } from '../../hooks/useProductos';
+import ItemList from '../ItemList/ItemList';
 
 const ItemListContainer = () => {
-  const [productos, setProductos] = useState([]);
-
-  useEffect(() => {
-    pedirDatos()
-      .then((res) => {
-        setProductos(res);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  const { productos } = useProductos()
 
   return (
     <div>
-      <h2>Item List Container</h2>
-      <p>Hola mundo</p>
-
-      <div className='row'>
-        {
-          productos.map((prod) => (
-            <div key={prod.id} className='col-3 m-2'>
-              <h4>{prod.nombre}</h4>
-              <img src={prod.img} alt={prod.nombre} />
-              <p>{prod.descripcion}</p>
-              <p>Precio: ${prod.precio}</p>
-              <button className='btn btn-primary'>Ver más</button>
-            </div>
-          ))
-        }
-      </div>
+        <ItemList productos={productos}/>
     </div>
   );
 };
---
+++ src/hooks/useProductos.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+import { pedirDatos } from "../helpers/pedirDatos"
+
+export const useProductos = () => {
+    const [productos, setProductos] = useState([])
+
+    useEffect(() => {
+        pedirDatos()
+            .then(r => setProductos(r))
+            .catch(e => console.log(e))
+    }, [])
+
+    return { productos }
+}
