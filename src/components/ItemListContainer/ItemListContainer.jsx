4a271f9382e8d4221698109c96e969da72656ec3
Matias Mattig <matu150503@gmail.com>
2023-08-12 22:26:51 -0300

    commit 10

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -5,11 +5,16 @@ import { useProductos } from '../../hooks/useProductos';
 import ItemList from '../ItemList/ItemList';
 
 const ItemListContainer = () => {
-  const { productos } = useProductos()
+  const { productos, loading } = useProductos()
+  console.log(loading, productos)
 
   return (
     <div>
-        <ItemList productos={productos}/>
+        {
+          loading
+              ? <h2>Cargando...</h2>
+              : <ItemList productos={productos}/>
+        }
     </div>
   );
 };
