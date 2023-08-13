e8a8ba72cd4f66d57cfb85587f8d24671f432a04
Matias Mattig <matu150503@gmail.com>
2023-08-12 22:25:28 -0300

    commit 9

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import './styles/styles.scss'
 import { Header } from './components/Header/Header'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
 
   return (
-    <div>
+    <BrowserRouter>
       <Header />
+
+      <Routes>
+        
+      </Routes>
+
       <ItemListContainer />
-    </div>
+    </BrowserRouter>
   )
 
 }
